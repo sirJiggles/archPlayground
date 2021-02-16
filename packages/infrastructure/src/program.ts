e5e83aef0a2079e83b8c74bff8560e0ab8919eb2
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2021-02-16 21:19:59 +0100

    set up pulumi to make the sns and sqs system for a simple test

--- packages/infrastructure/src/program.ts
+++ packages/infrastructure/src/program.ts
@@ -1,19 +1,21 @@
-import * as dotenv from 'dotenv'
-import { Lambda } from './lambda'
-import { api } from './api'
+import { sqsQueue, sqsQueuePolicy } from './sqs'
+import { snsSqsSubscription, snsTopic } from './sns'
 
 const program = () => {
-  dotenv.config({
-    path: process.env.PRODUCTION ? '.env.prod' : undefined,
-  })
+  // make a queue for the front end to poll, we can add more
+  // queues here if we wanted to, to allow us to listen in
+  // other services, for now let's just do the front end
+  const frontEndQueue = sqsQueue('frontEndQueue')
 
-  // create a simple little lambda
-  const lambda = Lambda()
-  // make an api gateway and use the lambda
-  api({
-    eventHandler: lambda,
-    name: 'arch-playground-api',
-  })
+  // make a topic and a subscription on the topic to the new queue
+  const topic = snsTopic('notificationService')
+  snsSqsSubscription('frontEndToSNS', topic, frontEndQueue.arn)
+
+  // now we have both the sns and the sqs set up
+  // we need to allow the sns to send the sqs queue events like so
+  sqsQueuePolicy('frontEndToSNSPolicy', frontEndQueue, topic)
+
+  // @TODO return what the stack will expose for the front end to poll
 }
 
 export default program
